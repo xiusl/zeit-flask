67fcf25bde542b56988679a6d34433bf259e8671
xiusl <xiushilin@hotmail.com>
2018-12-03 20:55:07 +0800

    cosr

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify, current_app 
 from models import User
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+
+    CORS(app, supports_credentials=True)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -27,6 +30,15 @@ def create_app():
         
         u = User.get_by_token(token)
         current_app.user = u
+
+    @app.after_request
+    def after_request(resp):
+        resp = make_response(resp)
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        resp.headers['Access-Control-Allow-Methods'] = 'GET,POST'
+        resp.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+        return resp
+
 
     return app
 
